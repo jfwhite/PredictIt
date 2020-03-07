805aeb9ac979276f248b2538204ad7a7b5db8956
JosephWhite <joseph@numinar.com>
2020-03-07 15:20:34 +0000

    sort by volatility

--- outlier_analysis.py
+++ outlier_analysis.py
@@ -21,16 +21,16 @@ def top_volatility(data):
     std_sell_no = contracts['sell_no'].std()
 
     print("Most volatile buy yes:")
-    print(std_buy_yes.sort_values(by=buy_yes, ascending=False).head())
+    print(std_buy_yes.sort_values(ascending=False).head())
 
     print("Most volatile sell yes:")
-    print(std_sell_yes.sort_values(by=sell_yes, ascending=False).head())
+    print(std_sell_yes.sort_values(ascending=False).head())
 
     print("Most volatile buy no:") 
-    print(std_buy_no.sort_values(by=buy_no, ascending=False).head())
+    print(std_buy_no.sort_values(ascending=False).head())
 
     print("Most volatile sell no:")
-    print(std_sell_no.sort_values(by=sell_no, ascending=False).head())
+    print(std_sell_no.sort_values(ascending=False).head())
 
 # Execute all analyses
 top_volatility(data)
