3dd911a0b8f0462e8ad28a00e1f49e761901880a
jfwhite <whitejosephf@gmail.com>
2020-03-07 09:24:12 +0000

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Chrome()
-driver.get("https://registrar.princeton.edu/course-offerings/")
-
-# I GUESS SELENIUM WILL HAVE TO WAIT
-# How else to improve quality? STEMness?
